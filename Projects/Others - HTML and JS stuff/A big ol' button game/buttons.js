bd0bf45e46260c9403bb92f3f8d975ea39371001
Alex Dias <alex.dias.gold@gmail.com>
2025-09-30 19:26:22 -0300

    Remake of button event listener formatting and clean up CSS for something

--- Projects/Others - HTML and JS stuff/A big ol' button game/buttons.js
+++ Projects/Others - HTML and JS stuff/A big ol' button game/buttons.js
@@ -1,21 +1,21 @@
-    const deleteParagraph = document.getElementById('deleteThisParagraph');
-    const deleteButton = document.getElementById('paragraphDeleter');
-    const addParagraph = document.getElementById('paragraphCreator');
-    let paragraphCounter = 0;
+const deleteParagraph = document.getElementById('deleteThisParagraph');
+const deleteButton = document.getElementById('paragraphDeleter');
+const addParagraph = document.getElementById('paragraphCreator');
+let paragraphCounter = 0;
 
-    deleteButton.addEventListener('click', () => {
-        const paragraphsToDelete = document.querySelectorAll('.deleteThisParagraph');
-        if (paragraphsToDelete.length > 0) {
-            paragraphCounter--;
-            paragraphsToDelete[paragraphsToDelete.length - 1].remove(); // Removes the last paragraph
-            paragraph.remove();
-        }
-    });
+deleteButton.addEventListener('click', () => {
+    const paragraphsToDelete = document.querySelectorAll('.deleteThisParagraph');
+    if (paragraphsToDelete.length > 0) {
+        paragraphCounter--;
+        paragraphsToDelete[paragraphsToDelete.length - 1].remove(); // Removes the last paragraph
+        paragraph.remove();
+    }
+});
 
-    addParagraph.addEventListener('click', () => {
-        paragraphCounter++;
-        const newParagraph = document.createElement('p');
-        newParagraph.textContent = `I am child number ${paragraphCounter}`;
-        newParagraph.classList.add('deleteThisParagraph');
-        document.body.appendChild(newParagraph);
-    })
+addParagraph.addEventListener('click', () => {
+    paragraphCounter++;
+    const newParagraph = document.createElement('p');
+    newParagraph.textContent = `I am child number ${paragraphCounter}`;
+    newParagraph.classList.add('deleteThisParagraph');
+    document.body.appendChild(newParagraph);
+})
